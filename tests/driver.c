0cb7e2e5f0c63250afd83ca043c1287eca8c9e3d
Dan Zimmerman <daniel.zimmerman@me.com>
2019-11-29 11:15:24 -0600

    Add basic ability to filter tests that run
    This is useful for profiling specific tests

--- tests/driver.c
+++ tests/driver.c
@@ -1,12 +1,13 @@
 // Copywrite (c) 2019 Dan Zimmerman
 
 #include <stdio.h>
+#include <unistd.h>
 
 #include "tests.h"
 
 int main(int argc, const char* argv[]) {
   (void)argc;
   (void)argv;
-  printf("Running frak tests...\n");
-  return run_tests() ? 0 : 1;
+  printf("Running frak tests... %d\n", getpid());
+  return run_tests(argc < 2 ? NULL : argv[1]) ? 0 : 1;
 }
