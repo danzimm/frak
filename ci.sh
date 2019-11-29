d7adfa87b0f0266e225af31e8972e911b89ea2eb
Dan Zimmerman <daniel.zimmerman@me.com>
2019-11-28 20:57:20 -0500

    Make ci.sh more robust (but wait its bash is that possible?), should fix travisci

--- ci.sh
+++ ci.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 set -ex
-mkdir -p build && pushd build && rm -r * && \
-  cmake ../ -G 'Unix Makefiles' -DCMAKE_BUILD_TYPE=Release && make && make test && \
-  popd
 
-mkdir -p asan && pushd asan && rm -r * && \
-  cmake ../ -G 'Unix Makefiles' -DCMAKE_BUILD_TYPE=Release -DFRAK_SAN_TYPE=address && make && make test && \
-  popd
+run_tests() {
+  build_dir="$1"
+  shift
+  rm -rf "$build_dir" && mkdir "$build_dir" && pushd "$build_dir" && \
+    cmake ../ -G 'Unix Makefiles' -DCMAKE_BUILD_TYPE=Release $@ && \
+    make && make test && \
+    popd
+}
+
+run_tests build
+run_tests asan -DFRAK_SAN_TYPE=address
