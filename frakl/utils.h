e10289cadedc401d185c074673f4593fc647f35f
Dan Zimmerman <daniel.zimmerman@me.com>
2019-12-02 19:24:42 -0500

    Implement function to round to next power of 2

---
+++ frakl/utils.h
@@ -0,0 +1,27 @@
+// Copywrite (c) 2019 Dan Zimmerman
+
+#pragma once
+
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#if UINTPTR_MAX == 0xFFFFFFFFFFFFFFFFu
+#define __round_clz __builtin_clzll
+#else
+#define __round_clz __builtin_clz
+#endif
+
+static inline uintptr_t round_to_next_power_of_two(uintptr_t x) {
+  if (!x) {
+    return 0;
+  }
+  const unsigned max_bit_set = sizeof(uintptr_t) * 8 - __round_clz(x) - 1;
+  assert(max_bit_set < sizeof(uintptr_t) * 8);
+  const uintptr_t max_bit_value = 1 << max_bit_set;
+  if (max_bit_value == x) {
+    return max_bit_value;
+  } else {
+    return max_bit_value * 2;
+  }
+}
---
+++ tests/utils.c
@@ -0,0 +1,17 @@
+// Copywrite (c) 2019 Dan Zimmerman
+
+#include <frakl/utils.h>
+
+#include "tests.h"
+
+TEST(RoundToNextPowerOfTwo) {
+  EXPECT_EQ(round_to_next_power_of_two(0), 0);
+  EXPECT_EQ(round_to_next_power_of_two(1), 1);
+  EXPECT_EQ(round_to_next_power_of_two(2), 2);
+  EXPECT_EQ(round_to_next_power_of_two(3), 4);
+  EXPECT_EQ(round_to_next_power_of_two(4), 4);
+  EXPECT_EQ(round_to_next_power_of_two(10), 16);
+  EXPECT_EQ(round_to_next_power_of_two(32), 32);
+  EXPECT_EQ(round_to_next_power_of_two(33), 64);
+  EXPECT_EQ(round_to_next_power_of_two(70000), 131072);
+}
