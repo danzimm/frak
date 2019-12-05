d84dfab0aa12a0fb08ce668dfd8e2413af79de2e
Dan Zimmerman <daniel.zimmerman@me.com>
2019-12-04 22:50:13 -0500

    Improve test infra to allow a format string to be passed through

--- tests/tests.h
+++ tests/tests.h
@@ -11,7 +11,7 @@ typedef void (*test_cb_t)(void);
 void register_test(const char* name, test_cb_t test, bool expect_fail,
                    const char* file, int line);
 bool run_tests(const char* filter);
-void fail(const char* check, const char* file, int line);
+void fail(const char* file, int line, const char* fmt, ...);
 
 #define TEST_(name, expect_fail)                                         \
   static void _test_##name(void);                                        \
@@ -23,11 +23,11 @@ void fail(const char* check, const char* file, int line);
 #define TEST(name) TEST_(name, false)
 #define TEST_FAIL(name) TEST_(name, true)
 
-#define EXPECT_(cond, failure)           \
-  do {                                   \
-    if (!(cond)) {                       \
-      fail(failure, __FILE__, __LINE__); \
-    }                                    \
+#define EXPECT_(cond, ...)                   \
+  do {                                       \
+    if (!(cond)) {                           \
+      fail(__FILE__, __LINE__, __VA_ARGS__); \
+    }                                        \
   } while (0)
 
 #define EXPECT_EQ(x, y) EXPECT_((x) == (y), #x " != " #y)
